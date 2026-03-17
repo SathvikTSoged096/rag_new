fddb233901ca93be6ef78b8346b398463c745bb7
Sathvik T Soged <139794350+SathvikTSoged096@users.noreply.github.com>
2026-03-17 23:19:47 +0530

    Update main.py

--- ai-service/main.py
+++ ai-service/main.py
@@ -1,47 +1,24 @@
-from fastapi import FastAPI, UploadFile, File
-from pydantic import BaseModel
-from pypdf import PdfReader
-import os
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-from rag_engine import add_documents, search
+documents = []
+vectorizer = TfidfVectorizer()
+vectors = None
 
-app = FastAPI()
+def add_documents(chunks):
+    global documents, vectors
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_DIR = os.path.join(BASE_DIR, "uploads")
+    documents.extend(chunks)
+    vectors = vectorizer.fit_transform(documents)
 
-os.makedirs(UPLOAD_DIR, exist_ok=True)
+def search(query):
+    if not documents:
+        return "No data uploaded yet"
 
-class Query(BaseModel):
-    question: str
+    query_vec = vectorizer.transform([query])
+    scores = cosine_similarity(query_vec, vectors)
 
-@app.get("/")
-def home():
-    return {"message": "AI service running"}
+    best_index = np.argmax(scores)
 
-@app.post("/upload")
-async def upload_pdf(file: UploadFile = File(...)):
-
-    file_path = os.path.join(UPLOAD_DIR, file.filename)
-
-    with open(file_path, "wb") as f:
-        f.write(await file.read())
-
-    reader = PdfReader(file_path)
-
-    text = ""
-    for page in reader.pages:
-        t = page.extract_text()
-        if t:
-            text += t
-
-     chunks = chunks = text.split("\n\n")
-
-    add_documents(chunks)
-
-    return {"message": "Uploaded successfully"}
-
-@app.post("/ask")
-def ask_ai(query: Query):
-    answer = search(query.question)
-    return {"answer": answer}
+    return documents[best_index]
