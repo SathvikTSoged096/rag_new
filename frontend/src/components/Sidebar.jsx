ac8a50675d2d34b3729ebc560c9a8d0b73d157b1
Sathvik T Soged <139794350+SathvikTSoged096@users.noreply.github.com>
2026-05-15 11:59:16 +0530

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -8,13 +8,7 @@ return(
 LMS
 </div>
 
-<button onClick={()=>setPage("dashboard")}className="menu-btn">Overview</button>
-<button onClick={()=>setPage("users")}className="menu-btn">All Users</button>
-<button onClick={()=>setPage("students")}className="menu-btn">Students</button>
-<button onClick={()=>setPage("instructors")}className="menu-btn">Instructors</button>
-<button onClick={()=>setPage("ranking")}className="menu-btn">Rankings</button>
-<button onClick={()=>setPage("stats")}className="menu-btn">Statistics</button>
-<button onClick={()=>setPage("logs")}className="menu-btn">Activity Logs</button>
+
 <button onClick={()=>setPage("upload")}className="menu-btn">Upload Notes</button>
 <button onClick={()=>setPage("ai")}className="menu-btn">AI Tutor</button>
 
